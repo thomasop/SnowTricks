540c5c8ae1a5befd25da8f7cf79fdfa7c588f633
thomasop <tdss33@hotmail.com>
2020-10-08 18:55:48 +0200

    Add: security system for first user, bug connect

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\User;
+use Doctrine\DBAL\Driver\Connection;
+use Symfony\Component\HttpFoundation\Response;
+use App\Form\UserLogType;
+use Symfony\Component\HttpFoundation\Request;
+
+class HomeController extends AbstractController
+{
+    /**
+     * @Route("/", name="home")
+     */
+    public function index()
+    {
+        return $this->json([
+            'message' => 'Welcome to your new controller!',
+            'path' => 'src/Controller/HomeController.php',
+        ]);
+    }
+
+    /**
+     * @Route("/user", name="user")
+     */
+    /*
+    public function user()
+    {
+        $entityManager = $this->getDoctrine()->getManager();
+        $user = new User();
+        $user->setPseudo('thomas');
+        $user->setPassword('$2y$13$tQDMscQXOnJikOTa21lxjeIPDPQq1StAE/Uo083MyPpFrtMAsfBWK');
+        $user->setEmail('tdss33@hotmail.fr');
+        $entityManager->persist($user);
+        $entityManager->flush();
+        return new Response('Saved new product with id '.$user->getId()); 
+    }
+    */
+}
--- src/Entity/User.php
+++ src/Entity/User.php
@@ -6,11 +6,12 @@ use App\Repository\UserRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Security\Core\User\UserInterface;
 
 /**
  * @ORM\Entity(repositoryClass=UserRepository::class)
  */
-class User
+class User implements UserInterface
 {
     /**
      * @ORM\Id
@@ -114,5 +115,58 @@ class User
         }
 
         return $this;
+    }
+    /**
+     * Returns the roles granted to the user.
+     *
+     *     public function getRoles()
+     *     {
+     *         return ['ROLE_USER'];
+     *     }
+     *
+     * Alternatively, the roles might be stored on a ``roles`` property,
+     * and populated in any number of different ways when the user object
+     * is created.
+     *
+     * @return string[] The user roles
+     */
+    public function getRoles(): array
+    {
+        return ['ROLE_USER'];
+    }
+
+    
+
+    /**
+     * Returns the salt that was originally used to encode the password.
+     *
+     * This can return null if the password was not encoded using a salt.
+     *
+     * @return string|null The salt
+     */
+    public function getSalt()
+    {
+        return null;
+    }
+
+    /**
+     * Returns the username used to authenticate the user.
+     *
+     * @return string The username
+     */
+    public function getUsername(): string
+    {
+        return (string) $this->email;
+    }
+
+    /**
+     * Removes sensitive data from the user.
+     *
+     * This is important if, at any given point, sensitive information like
+     * the plain-text password is stored on this object.
+     */
+    public function eraseCredentials(): void
+    {
+
     }
 }
