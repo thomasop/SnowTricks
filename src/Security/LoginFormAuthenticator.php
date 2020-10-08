1365fb4bf8e9f9b0f3cb23c5d1d7865e467beba9
thomasop <tdss33@hotmail.com>
2020-10-08 23:13:08 +0200

    Edit: user connect debug but logout bug

--- src/Security/LoginFormAuthenticator.php
+++ src/Security/LoginFormAuthenticator.php
@@ -114,7 +114,8 @@ class LoginFormAuthenticator extends AbstractFormLoginAuthenticator implements P
 
         // For example : return new RedirectResponse($this->urlGenerator->generate('some_route'));
         //throw new \Exception('TODO: provide a valid redirect inside '.__FILE__);
-        return new RedirectResponse($this->urlGenerator->generate('http://localhost:8000/login'));
+        //return new RedirectResponse($this->urlGenerator->generate('app_login'));
+        echo'ok';
     }
 
     protected function getLoginUrl()
