0f053bf4156bf03f432b717890f6e3a008704069
thomasop <tdss33@hotmail.com>
2020-10-11 10:36:52 +0200

    Edit: add link for comment page

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -25,37 +25,4 @@ class HomeController extends AbstractController
             'trick' => $trick
             ]);
     }
-
-    /**
-     * @Route("/user/{id}", name="user")
-     */
-    public function user($id)
-    {
-        $show = $this->getDoctrine()
-        ->getRepository(User::class)
-        ->find($id);
-        //dd($id);
-        
-        
-        $entityManager = $this->getDoctrine()->getManager();
-        //$user = new User();
-        $trick = new Trick();
-       // $ok = $user->getId();
-        //dd($ok);
-        
-        $trick->setUser($show);
-        $trick->setName('tail slide');
-        $trick->setDescription('arrière de la planche sur la barre');
-        $trick->setType('slides');
-        $trick->setPicture('tail slide picture');
-        $trick->setVideo('tail slide vidéo');
-        $entityManager->persist($trick);
-        $entityManager->flush();
-        
-        return $this->render('home/home.html.twig', [
-            'trick' => $trick
-            ]);
-        
-       
-    }
 }
