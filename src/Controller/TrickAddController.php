576e5eddb1934533864b564ed08df2999946dacf
thomasop <tdss33@hotmail.com>
2020-10-30 17:42:16 +0100

    Edit: trick add controller clean

--- src/Controller/TrickAddController.php
+++ src/Controller/TrickAddController.php
@@ -21,17 +21,14 @@ class TrickAddController extends AbstractController
         // creates a task object and initializes some data for this example
     /** @var EntityManagerInterface */
     private $entityManager;
-    //private $request;
 
     
     public function __construct(
         EntityManagerInterface $entityManager,
         TokenStorageInterface $tokenStorage
-        //Request $request
     ) {
         $this->entityManager = $entityManager;
         $this->tokenStorage = $tokenStorage;;
-        //$this->request = $request;
     }
 
 
@@ -40,22 +37,15 @@ class TrickAddController extends AbstractController
     */
     public function trickAdd(Request $request, SluggerInterface $slugger)
     {
-        //$this->denyAccessUnlessGranted('IS_AUTHENTICATED_FULLY');
-
         $trick = new Trick();
         $form = $this->createForm(TrickType::class, $trick);
-        
-        
         $form->handleRequest($request);
         
         if ($form->isSubmitted() && $form->isValid()) {
-            
             $images = $form->get('picture')->getData();
-
             if ($images === null) {
                 $images = 'default.jpg';
                 $trick->setImage($images);
-                //dd($image);
             }
             else {
                 foreach($images as $image){
@@ -78,44 +68,8 @@ class TrickAddController extends AbstractController
                     $img->setName($newFilename);
                     $img->setTrickId($trick);
                     $this->entityManager->persist($img);
-               // $img = new Image();
-                //$img->setName($newFilename);
-                //dd($img);
-                }
-                //$trick->addImage($img);
-                
+                }                
             }
-
-            // On récupère les images transmises
-            /*
-            $images = $form->get('images')->getData();            
-            //dd($images);
-            // On boucle sur les images
-            foreach($images as $image){
-                
-                //$newFilename = $this->fileUploader->upload($image);
-                $originalFilename = pathinfo($image->getClientOriginalName(), PATHINFO_FILENAME);
-                // this is needed to safely include the file name as part of the URL
-                $safeFilename = $slugger->slug($originalFilename);
-                $newFilename = $safeFilename.'-'.uniqid().'.'.$image->guessExtension();
-                try {
-                    $image->move(
-                        $this->getParameter('pictures_directory'),
-                        $newFilename
-                    );
-                } catch (FileException $e) {
-                // ... handle exception if something happens during file upload
-                }
-                // On crée l'image dans la base de données
-                $img = new Image();
-                $img->setName($newFilename);
-                $img->setTrickId($trick);
-                $this->entityManager->persist($img);
-                //dd($img);
-                //$trick->addImage($img);
-
-            }
-            */
             $videos = $form->get('video')->getData();
 
             if ($videos !== null) {
@@ -126,8 +80,6 @@ class TrickAddController extends AbstractController
                     $video->setTrickId($trick);
 
                     $this->entityManager->persist($video);
-                    //$trick->addVideo($video);
-                    //dd(addVideo($video));
                 }
             }
             $trick->setUser($this->tokenStorage->getToken()->getUser());
@@ -136,63 +88,11 @@ class TrickAddController extends AbstractController
             $this->entityManager->persist($trick);
             $this->entityManager->flush();
             
-            //$this->session->getFlashBag()->add("success", "Trick créé !");
-            
-         
-    
-            
             return $this->redirectToRoute('home');
         }
         return $this->render('form/formtrick.html.twig', [
             'form' => $form->createView(),
             'trick' => $trick
             ]);
-        //return $this->responder->response($form);
     }
-        /*
-        $trick = new Trick();
-        //$task->setTask('Write a blog post');
-        //$task->setDueDate(new \DateTime('tomorrow'));
-
-
-        $form = $this->createForm(TrickType::class, $trick);
-        $form->handleRequest($request);
-        if ($form->isSubmitted() && $form->isValid()) {
-    // $form->getData() holds the submitted values
-    // but, the original `$task` variable has also been updated
-            $pictureFile = $form->get('picture')->getData();
-            if ($pictureFile) {
-                $originalFilename = pathinfo($pictureFile->getClientOriginalName(), PATHINFO_FILENAME);
-                // this is needed to safely include the file name as part of the URL
-                $safeFilename = $slugger->slug($originalFilename);
-                $newFilename = $safeFilename.'-'.uniqid().'.'.$pictureFile->guessExtension();
-                try {
-                    $pictureFile->move(
-                        $this->getParameter('pictures_directory'),
-                        $newFilename
-                    );
-                } catch (FileException $e) {
-                // ... handle exception if something happens during file upload
-                }
-                $trick->setPicture($newFilename);
-            }
-    
-    
-            //$trick = $form->getData();
-
-            // ... perform some action, such as saving the task to the database
-            // for example, if Task is a Doctrine entity, save it!
-            $entityManager = $this->getDoctrine()->getManager();
-            $entityManager->persist($pictureFile);
-            $entityManager->flush();
-
-            return $this->redirectToRoute('home');
-        }
-        return $this->render('form/formtrick.html.twig', [
-            'form' => $form->createView()
-            ]);
-        // ...
-        */
-        
-    
 }
