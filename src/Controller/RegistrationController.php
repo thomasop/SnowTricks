59e1f1d767da75fc3e3fe193d2376854caf0302e
thomasop <tdss33@hotmail.com>
2020-11-01 09:02:46 +0100

    Add: Email send when registration is down with link

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -11,25 +11,27 @@ use App\Responders\RegistrationResponder;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\User\UserInterface;
 use Symfony\Component\String\Slugger\SluggerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+use App\Repository\UserRepository;
 
 class RegistrationController extends AbstractController
 {
+
     public function __construct(
         
         UserPasswordEncoderInterface $passwordEncoder
     
     ){
-
         $this->passwordEncoder = $passwordEncoder;
     }
 
     /**
      * @Route("/register", name="app_register")
      */
-    public function new(Request $request, SluggerInterface $slugger)
+    public function new(Request $request, SluggerInterface $slugger, \Swift_Mailer $mailer)
     {
         // just setup a fresh $task object (remove the example data)
         $user = new User();
@@ -82,8 +84,27 @@ class RegistrationController extends AbstractController
             // for example, if Task is a Doctrine entity, save it!
             $user->setRoles(['ROLE_ADMIN']);
             $entityManager = $this->getDoctrine()->getManager();
-            $entityManager->persist($user);
-            $entityManager->flush();
+            $message = (new \Swift_Message('Hello Email'))
+            ->setFrom('thomasdasilva010@gmail.com')
+            ->setTo('thomasdasilva010@gmail.com')
+            ->setBody(
+                $this->renderView(
+                    // templates/emails/registration.html.twig
+                    'security/email.html.twig',
+                    ['user' => $user]
+                ),
+                'text/html'
+            )
+
+        // you can remove the following code if you don't define a text version for your emails
+        
+    ;
+
+    $mailer->send($message);
+
+    //return $this->render(...);
+            //$entityManager->persist($user);
+            //$entityManager->flush();
 
             return $this->redirectToRoute('app_login');
         }
@@ -92,4 +113,33 @@ class RegistrationController extends AbstractController
             'form' => $form->createView(),
         ]);
     }
+
+    /**
+     * @Route("/verify/email", name="app_verify_email")
+     */
+    public function verifyUserEmail($id, UserRepository $userRepository, Request $request): Response
+    {
+        // $this->denyAccessUnlessGranted('IS_AUTHENTICATED_FULLY');
+        $user = $userRepository
+        ->find($id);
+        // On valide l'email
+        
+        try {
+            $entityManager = $this->getDoctrine()->getManager();
+            //$this->emailVerifier->handleEmailConfirmation($request, $this->getUser());
+            $user->setStatus('1');
+
+            $this->entityManager->persist($user);
+            $this->entityManager->flush();
+        } catch (VerifyEmailExceptionInterface $exception) {
+            return $this->redirectToRoute('app_register');
+        }
+
+        //$this->addFlash('success', 'Your email address has been verified.');
+
+        return $this->redirectToRoute('app_login');
+    }
+
+
+    
 }
