d632d949a2f1b4b317fdcb5f5f1f507efe5117a3
thomasop <tdss33@hotmail.com>
2020-12-16 16:18:46 +0100

    Add: search function

---
+++ src/Tool/Search.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\tool;
+
+class Search
+{	
+	public function search()
+	{
+		return $this->_path;
+    }
+
+    public function searchName()
+	{
+		return $this->_path;
+	}
+}     
